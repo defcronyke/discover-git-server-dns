a2779b99a059ffb0e73122a99784413dd73c725b
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-18 16:21:56 -0400

    Fix git-subnet.sh

--- git-subnet.sh
+++ git-subnet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ip route ls | grep "`ip route ls | grep default | head -n 1 | sed 's/^.* dev \(.*\) proto .*$/\1/g'`" | tail -n +2 | head -n 1 | awk '{print $1}'
+ip route ls | grep "`./git-nic.sh`" | tail -n +2 | head -n 1 | awk '{print $1}'
 
