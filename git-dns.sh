d8cb2a80741f15b1ed8f6ce3f556aabe4c63e1c8
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-18 16:49:49 -0400

    Fix git-dns.sh

--- git-dns.sh
+++ git-dns.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-outfile="hosts-$(date +%s.%N).txt"; nmap --unprivileged --open -nPn -oG "$outfile" --disable-arp-ping --discovery-ignore-rst $(./git-subnet.sh) -e $(./git-nic.sh) -p 53 >/dev/null 2>&1; cat "$outfile" | grep "open" | tail -n +2 | awk '{print $2}' | sort | uniq; rm "$outfile"
+for i in `./git-subnet.sh`; do
+	outfile="hosts-$(date +%s.%N).txt"; nmap --unprivileged --open -nPn -oG "$outfile" --disable-arp-ping --discovery-ignore-rst $i -p 53 >/dev/null 2>&1; cat "$outfile" | grep "open" | tail -n +2 | awk '{print $2}' | sort | uniq; rm "$outfile"
+done
 
