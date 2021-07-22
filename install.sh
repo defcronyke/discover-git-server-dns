75793fd940ebdfb96bca4dd6a1095eacfc666f3d
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-22 00:48:34 -0400

    Install dependencies.

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ discover_git_server_dns_install() {
   echo ""
 
   sudo apt-get update && \
-  sudo apt-get install -y bind9
+  sudo apt-get install -y bind9 nmap
 
   # Install zone file: /etc/bind/db.git
   echo ""
