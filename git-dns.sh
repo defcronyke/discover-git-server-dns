716370a518880e2e322a232b4369e4c4c85a0263
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-21 22:25:52 -0400

    Improve output.

--- git-dns.sh
+++ git-dns.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 for i in `./git-subnet.sh`; do
-	outfile="hosts-$(date +%s.%N).txt"; nmap --unprivileged --open -nPn -oG "$outfile" --disable-arp-ping --discovery-ignore-rst $i -p 53 >/dev/null 2>&1; cat "$outfile" | grep "open" | tail -n +2 | awk '{print $2}' | sort | uniq; rm "$outfile"
+	outfile="hosts-$(date +%s.%N).txt"; nmap --unprivileged --open -nPn -oG "$outfile" --disable-arp-ping --discovery-ignore-rst $i -p 53 >/dev/null 2>&1; cat "$outfile" 2>/dev/null | grep "open" | tail -n +2 | awk '{print $2}' 2>/dev/null | sort | uniq; rm "$outfile" 2>/dev/null
 done
-
