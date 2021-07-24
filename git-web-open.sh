1c3c564f704d836986aea77b09bfeec3c0f9cfb0
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-24 13:44:16 -0400

    Detect user-supplied git servers.

--- git-web-open.sh
+++ git-web-open.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-for i in `./git-web.sh`; do
+for i in `./git-web.sh $@`; do
 	echo "$i"
 	xdg-open "$i" 2>/dev/null
 done
-
--- git-web.sh
+++ git-web.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+GC_NEW_GIT_SERVERS_TEST=( )
+GC_NEW_GIT_SERVERS=( )
+
+for i in $@; do
+  gc_new_git_server_hostname="$(echo "$i" | cut -d: -f1 | cut -d@ -f2)"
+  cat "$HOME/.ssh/config" 2>/dev/null | grep "$gc_new_git_server_hostname" | grep -i hostname >/dev/null 2>&1 && gc_new_git_server_hostname="$(cat "$HOME/.ssh/config" 2>/dev/null | grep "$gc_new_git_server_hostname" | grep -i hostname | xargs 2>/dev/null | awk '{print $2}' 2>/dev/null)"
+
+  GC_NEW_GIT_SERVERS_TEST+=( "$(printf 'http://%s:1234\n' "$gc_new_git_server_hostname")" )
+done
+
+for i in ${GC_NEW_GIT_SERVERS_TEST[@]}; do
+  curl -m 3 "$i" >/dev/null 2>&1 && \
+    GC_NEW_GIT_SERVERS+=( "$i" )
+done
+
+for i in ${GC_NEW_GIT_SERVERS[@]}; do
+  echo "$i"
+done
+
 for i in "`./git-srv.sh`"; do
-	echo "$i" | sed 's/\.$//g' | awk '{print "http://"$NF":"$(NF-1)"/"}' 2>/dev/null
+	echo "$i" | sed 's/\.$//g' | awk '{print "http://"$NF":"$(NF-1)}' 2>/dev/null
 done
