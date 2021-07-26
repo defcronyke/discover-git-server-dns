71597a5995350e82abc6691bc867773ee217c257
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-26 17:46:51 -0400

    Improve git server detection.

--- git-update-srv.sh
+++ git-update-srv.sh
@@ -6,9 +6,9 @@
 gc_dns_git_server_update_srv_records_git() {
   cat db.git | grep "_git\._tcp" | sort | uniq | tee db.git.next.tmp
   
-  if [ "basename $(echo "$PWD")" != "gitcid" ]; then
-    sudo cat /etc/bind/db.git | grep "_git\._tcp" | sort | uniq | tee -a db.git.next.tmp
-  fi
+  # if [ "basename $(echo "$PWD")" != "gitcid" ]; then
+  cat /etc/bind/db.git | grep "_git\._tcp" | sort | uniq | tee -a db.git.next.tmp
+  # fi
     
   for i in ${gc_update_servers[@]}; do
     echo "_git._tcp  IN      SRV     $i" | tee -a db.git.next.tmp
@@ -35,7 +35,7 @@ gc_dns_git_server_update_srv_records() {
   gc_update_servers=( )
   gc_update_servers_hostnames=( )
 
-  for i in $@; do
+  for i in "$@"; do
     gc_update_servers+=( "$(./git-srv.sh "$i" | grep " 1234 ")" )
     gc_update_servers_hostnames+=( "$i" )
   done
