ce0e4dd50ad3d4732647d9e049269a75a73850bc
Jeremy Carter <jeremy@jeremycarter.ca>
2021-08-08 07:54:59 -0400

    Improve service discovery.

--- git-update-srv.sh
+++ git-update-srv.sh
@@ -64,8 +64,8 @@ gc_dns_git_server_update_srv_records_git() {
   echo " | DEBUG |"
 
   # Remove old self hostname from zone file.
-  sed -i "s/^$(hostname)\.?\s*IN\s*A\s*.*\.?$//g" ${current_bind_dir}/db.git
-  sed -i "s/^$(hostname)\.?\s*IN\s*A\s*.*\.?$//g" db.git
+  sed -i "s/^\$(hostname)\.?\s*IN\s*A\s*.*\.?$//g" ${current_bind_dir}/db.git
+  sed -i "s/^\$(hostname)\.?\s*IN\s*A\s*.*\.?$//g" db.git
   sed -i "s/^@\s*IN\s*A\s*.*\.?$//g" db.git
   
   cat ${current_bind_dir}/db.git | grep -P "^.+\.?[[:space:]]+IN[[:space:]]+A[[:space:]]+.+\.?$" | sort | uniq | \
@@ -76,7 +76,7 @@ gc_dns_git_server_update_srv_records_git() {
 
   if [ -f ${current_bind_dir}/db.git.a.old ]; then
     # Remove old self hostname from zone file.
-    sed -i "s/^$(hostname)\.?\s*IN\s*A\s*.*\.?$//g" ${current_bind_dir}/db.git.a.old
+    sed -i "s/^\$(hostname)\.?\s*IN\s*A\s*.*\.?$//g" ${current_bind_dir}/db.git.a.old
 
     cat ${current_bind_dir}/db.git.a.old | sort | uniq | \
     tee -a ${current_bind_dir}/db.git.a.next
@@ -850,7 +850,7 @@ gc_dns_git_server_update_srv_records() {
   echo " | DEBUG |"
 
   # Remove old self hostname from zone file.
-  sed -i "s/^$(hostname)\.?\s*IN\s*A\s*.*\.?$//g" db.git
+  sed -i "s/^\$(hostname)\.?\s*IN\s*A\s*.*\.?$//g" db.git
   sed -i "s/^@\s*IN\s*A\s*.*\.?$//g" db.git
 
   cat db.git | grep -P "^.+\.?[[:space:]]+IN[[:space:]]+A[[:space:]]+.+\.?$" | sort | uniq | \
@@ -858,7 +858,7 @@ gc_dns_git_server_update_srv_records() {
 
   if [ -f db.git.a.old ]; then
     # Remove old self hostname from zone file.
-    sed -i "s/^$(hostname)\.?\s*IN\s*A\s*.*\.?$//g" db.git.a.old
+    sed -i "s/^\$(hostname)\.?\s*IN\s*A\s*.*\.?$//g" db.git.a.old
     sed -i "s/^@\s*IN\s*A\s*.*\.?$//g" db.git.a.old
 
     cat db.git.a.old | sort | uniq | \
